8d8bd1698eb281f47d9e39e9afabdc704988fe4d
tunombreJason <edisongaiborconde@hotmail.com>
2018-05-31 10:15:04 -0500

    Agregando ejercicios

---
+++ Arreglo1/src/arreglo1/Arreglo1.java
@@ -0,0 +1,16 @@
+
+package arreglo1;
+
+public class Arreglo1 {
+
+    public static void main(String[] args) {
+        int[]c=new int[10];
+        c[0]=10;
+        for(int contador=0;contador<c.length;contador++){
+            System.out.println(c[contador]);
+            
+        }
+        
+    }
+    
+}
